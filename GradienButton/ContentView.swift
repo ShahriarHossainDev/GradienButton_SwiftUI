f04afa6fbf02977f6885c4930c27f9c2966b3ec9
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2024-09-26 18:10:53 +0600

    Initial Commit

---
+++ GradienButton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GradienButton
+//
+//  Created by Shishir_Mac on 26/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GradienButton/GradienButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradienButtonApp.swift
+//  GradienButton
+//
+//  Created by Shishir_Mac on 26/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GradienButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
